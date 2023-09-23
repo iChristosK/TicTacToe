6bbb6efa1d32e6021a7488d2f35456315914c476
iChristosK <16455384+iChristosK@users.noreply.github.com>
2023-09-23 19:03:54 +0200

    Updated TSX and PXD

--- TicTacToeApp/App.tsx
+++ TicTacToeApp/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+
 import { TicTacToe } from './src/components/organisms/TicTacToe/TicTacToe';
 import store from './src/store';
 
